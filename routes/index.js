38a378398fcead82d66c08f368acf1e15a3ee13f
Browntth <browntth@icloud.com>
2021-04-29 22:10:26 -0500

    V 2.5.4.0
    Agregue la interfaz de /detallesCurso para que Aaron pudiera hacer las pruebas de la arquitectura

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const router = express.Router();
 // -------modulos de los controller-------
 const inicioController = require('../controllers/generalController');
 const admiController = require('../controllers/admiController');
+const alumnoController = require('../controllers/alumnoController');
 const pruebaController = require('../controllers/pruebaController');
 
 // -------LIBRO DE RUTAS-------
@@ -20,6 +21,7 @@ router.get('/ejemplo', admiController.postEjemplo);
 //vistas del ponente
 
 //vistas del alumno
+router.get('/detallesCurso', alumnoController.postDetallesCurso);
 
 //vistas de prueba
 router.get('/primeraPrueba', pruebaController.postPrimeraPrueba);
