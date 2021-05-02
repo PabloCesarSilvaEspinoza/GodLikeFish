0da0c5026b4259d207c20da61b56efdfaee38183
GayshaxD <62309533+GayshaxD@users.noreply.github.com>
2021-05-02 01:40:32 -0500

    v2.7.3.1
    Se modificaron 3 archivos de la carpeta course(Todo fue probado):
    
    controller: Se crearon 2 funciones. insert y update. Para los procesos almacenados.
    
    network: Se crearon 3 metodos para llamar las funciones  de render.
    
    render: Se crearon 3 metodos, postAgregar, putEditar  y getCurso. las 3 ya funcionan.

--- components/course/network.js
+++ components/course/network.js
@@ -3,6 +3,10 @@ const express = require('express');
 const router = express.Router();
 const render = require('./render');
 
+router.get('/', render.getCursos);
 router.get('/:id', render.postDetallesCurso);
+router.post('/', render.postAgregarCurso);
+router.put('/', render.putEditarCurso)
+
 
 module.exports = router;
