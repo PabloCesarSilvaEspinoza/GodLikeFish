03438a0b52fc818f91b14e9c00cc7740284e79fb
Mguadalupe291 <83089535+Mguadalupe291@users.noreply.github.com>
2021-05-05 15:00:33 -0500

    v 2.1.10.1
    Datos en corchete(aun no resuelto)

--- components/course/render.js
+++ components/course/render.js
@@ -3,23 +3,58 @@ const Controller = require('./index');
 
 module.exports = {
 
-    getVerCursos:async function(req, res, next){
-        const courses= await Controller.list();
-     },
+    getVerCursos: async function (req, res, next) {
+        const courses = await Controller.list();
+    },
 
-    getVerCurso: async function(req, res, next){
+    getVerCurso: async function (req, res, next) {
         const id = req.params.id;
         const course = await Controller.get(id);
         res.render('course/detallesCurso', course[0]);
     },
 
-    postAgregarCurso: async function(req, res, next){
+    postAgregarCurso: async function (req, res, next) {
         await Controller.insert(req.body);
-       res.redirect('/');
+        res.redirect('/');
     },
 
-    putEditarCurso: async function(req, res, next){
+    putEditarCurso: async function (req, res, next) {
         await Controller.update(req.body);
-       res.redirect('/');
+        res.redirect('/');
+    },
+    getPrincipalAdministradorPonentes: async function (req, res, next) {
+        res.render('administrador/PrincipalAdministrador-Ponentes', {
+            datatables: true,
+            
+            ponentes: [{
+            nombre: "Amelia Rojas",
+            cargo: "Profesor 2",
+            area: "Medica",
+            edad:"39",
+            antiguedad:"4"
+            }],
+            etiquetasPonentes:[{
+                ponentesRegistrados:"2",
+                ponentesConCurso:"78",
+                ponentesSinCurso:"8"
+            }],
+            datosResumenPonentes:[{
+                resumenRegistrados:"90",
+                resumenActivos:"34",
+                resumenInactivos:"90"
+            }]
+
+        });
+    },
+    getPrincipalAdministrador: async function (req, res, next) {
+        res.render('administrador/PrincipalAdministrador', {
+    
+            etiquetasPonentes:[{
+                ponentesRegistrados:"2",
+                ponentesConCurso:"78",
+                ponentesSinCurso:"8"
+            }],
+            
+        });
     },
 };
