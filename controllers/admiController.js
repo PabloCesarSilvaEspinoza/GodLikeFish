8656707f427847d5bd25bda2be2941e8176d5f4f
AaronIPR <aaron.ipr@hotmail.com>
2021-05-14 22:10:13 -0500

    Revert "Merge branch 'Sunday' into trial"
    This reverts commit dcafd643cffbbb4e7889d3df3024a41f49327c43, reversing
    changes made to 160829757076dc840dc57103d931a3cb7f479bae.

--- controllers/admiController.js
+++ controllers/admiController.js
@@ -1,22 +1,5 @@
-//todo lo que requieras va aqui
 const Controller = require('../components/user/index');
-// ejemplo: la base de datos (pool), bcrypt, helpers, etc.
-//const procedimiento = require('../database/procedimientosInicio');
-
-//aqui se exportan las funciones 
 module.exports = {
-
-    postTarjetasPonentes: async function(req, res, next){
-        res.render('administrador/TarjetasDePonentes', {
-
-        });
-    },
-
-    postEditarPonente: async function(req, res, next){
-        res.render('administrador/EditarPonente', {
-        
-        }
-    )},
 
     postEjemplo: async function(req, res, next){
         const users = await Controller.list();
@@ -65,76 +48,11 @@ module.exports = {
 
         });
     },
-    postAdministrador: async function(req, res, next){
-        res.render('administrador/PrincipalAdministrador', {
-
-        });
-    },
+    
     postDashboard: async function(req, res, next){
-        res.render('administrador/dashboard',{
-            tarjetasDashboard:[
-                {
-                    usuariosRegistrados:"",
-                    reconocimientosEntregados:"",
-                    cursosRegistrados:"",
-                    ponentesRegistrados:""
-                }
-            ],
-            usuariosNoVerificados:[
-                {
-                    cantidad:"",
-                    usuario:[
-                        {
-                            imagen:"",
-                            nombre:"",
-                            Area:"",
-                            Puesto:"",
-                            Antiguedad:"",
-                            matricula:""
-                        }
-                    ]
-                }
-            ],
-            carruselCentral:[
-                {
-                    cursos:[
-                        {
-                            registrados:"",
-                            activos:"",
-                            inactivos:""
-                        }
-                    ],
-                    alumnos:[
-                        {
-                            registrados:"",
-                            certificados:"",
-                            sinCertificado1:""
-                        }
-                    ],
-                    ponentes:[
-                        {
-                            registrados:"",
-                            activos:"",
-                            inactivos:""
-                        }
-                    ]
-                }
-            ],
-            reporteErrores:[
-                {
-                    cantidad:"",
-                    reporte:[
-                        {
-                            imagenUsuario:"",
-                            nombreUsuario:"",
-                            contenidoReporte:"",
-                            fechaReporte:"",
-                            estado:""
-                        }
-                    ]
-                }
-            ]
-
-        })
+        const users = await Controller.list();
+        res.render('administrador/dashboard', {
+            users,
+            dataTablesExport: true})
     }
 };
--- controllers/pruebaController.js
+++ controllers/pruebaController.js
@@ -1,8 +1,3 @@
-//todo lo que requieras va aqui
-// ejemplo: la base de datos (pool), bcrypt, helpers, etc.
-const procedimiento = require('../database/procedimientosInicio');
-
-//aqui se exportan las funciones 
 module.exports = {
     postPrimeraPrueba: async function(req, res, next){
         res.render('prueba/primeraPrueba',{
