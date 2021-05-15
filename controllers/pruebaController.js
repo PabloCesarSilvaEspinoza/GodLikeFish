e61c6d879c3c4a732f5717d7733ce3dc0f6fd4c2
Browntth <browntth@icloud.com>
2021-05-15 13:57:42 -0500

    Revert "Revert "Merge branch 'Sunday' into trial""
    This reverts commit 8656707f427847d5bd25bda2be2941e8176d5f4f.

--- controllers/pruebaController.js
+++ controllers/pruebaController.js
@@ -1,3 +1,8 @@
+//todo lo que requieras va aqui
+// ejemplo: la base de datos (pool), bcrypt, helpers, etc.
+const procedimiento = require('../database/procedimientosInicio');
+
+//aqui se exportan las funciones 
 module.exports = {
     postPrimeraPrueba: async function(req, res, next){
         res.render('prueba/primeraPrueba',{
