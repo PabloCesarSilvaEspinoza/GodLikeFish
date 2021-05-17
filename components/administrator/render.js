0d5d3fe81003820ddb6bdc159db1092af1d6381c
AaronIPR <aaron.ipr@hotmail.com>
2021-05-16 22:27:27 -0500

    v3.4.7.2
    -Creación de carpetas para el curso.

--- components/administrator/render.js
+++ components/administrator/render.js
@@ -1,8 +1,29 @@
 const Controller = require('./index');
+const fs = require('fs');
 
 path = require('path')
 
-
+async function crearCarpetaCurso (cursoID) {
+    const raiz = path.join(__dirname, '../../');
+    const direccion = `${raiz}multimedia/courses/${cursoID}`
+    if(!fs.existsSync(direccion)){
+        fs.mkdir(direccion, error => {
+            if (error) {
+                console.log(error.message);
+            }
+        });
+        fs.mkdir(direccion + '/resources', error => {
+            if(error){
+                console.log(error.message);
+            }
+        });
+        fs.mkdir(direccion + '/homewroks', error => {
+            if(error){
+                console.log(error.message);
+            }
+        });
+    }
+}
 
 module.exports = {
     getAdministrarCursos: async function(req, res, next){
@@ -13,8 +34,10 @@ module.exports = {
         });
     },
     postAgregarCurso: async function (req, res, next){
-        const responseDB = await Controller.insertCourse(req.body);
-        const courseID = responseDB[0][0].ID;
+        //const responseDB = await Controller.insertCourse(req.body);
+        //const courseID = responseDB[0][0].ID;
+        console.log(req.body);
+        crearCarpetaCurso(8);
         res.redirect('/admin/administrarCursos')
-    }
+    },
 };
