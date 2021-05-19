67d49d5e7e99b30843194394b50f4c6b2c6c4bd8
Browntth <browntth@icloud.com>
2021-05-19 11:04:18 -0500

    v 3.5.10.2
    implementando passport, no redirecciona ni nada :'c

--- components/auth/network.js
+++ components/auth/network.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const passport = require('passport');
 const render = require('./render');
 const response = require('../../network/response');
 const Controller = require('./index');
@@ -9,15 +10,19 @@ router.get('/', render.postPaginaPrincipal);
 router.get('/confirmarCorreo', render.postConfirmarCorreo);
 
 router.post('/login', function(req,res){
-    console.log(req.body);
-    Controller.login(req.body.correoUsuario, req.body.passwordUsuario)
+    //console.log(req.body);
+    passport.authenticate(
+        Controller.login(req.body.correoUsuario, req.body.passwordUsuario)
         .then(token => {
             response.success(req, res, token, 200);
+            console.log(res.user);
             //console.log(res.correoUsuario, res.passwordUsuario, token);
         })
         .catch(error => {
             response.error(req, res, 'Informacion invalida', 400);
-        })
+        }),
+        { successRedirect: '/primeraPrueba', failureRedirect: '/login' }
+    )
 });
 
 
