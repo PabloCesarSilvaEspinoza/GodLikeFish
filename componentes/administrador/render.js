7655340ceaeb70842962e35aba099310f0e306e3
Browntth <browntth@icloud.com>
2021-05-20 19:34:36 -0500

    v 3.5.11.1
    caso especial, modificando trial comprobando los cambios de las sesiones

--- componentes/administrador/render.js
+++ componentes/administrador/render.js
@@ -1,15 +1,22 @@
 const Controller = require('./index');
+const fs = require('fs');
+
+path = require('path')
 
 module.exports = {
-    postPaginaPrincipal: async function(req, res, next){
-        res.render('general/paginaPrincipal',{
-            general:true,
-            select2:true
+    getAdministrarCursos: async function(req, res, next){
+        const cursos = await Controller.listCursos();
+        res.render('course/administrarCursos',{
+            dataTablesExport:true,
+            cursos
         });
     },
-    postConfirmarCorreo: async function(req, res, next){
-        res.render('general/confirmarCorreo',{
-            general: true
-        });
-    }
+    postAgregarCurso: async function (req, res, next){
+        const respuestaBD = await Controller.insertCurso(req.body);
+        const cursoID = respuestaBD[0][0].ID;
+        const fotoCurso = `${cursoID}/${req.files.fotoCurso[0].originalname}`;
+        const temarioCurso = `${cursoID}/${req.files.temarioCurso[0].originalname}`;
+        await Controller.insertMultimediaCurso(cursoID, temarioCurso, fotoCurso)
+        res.redirect('/admin/administrarCursos')
+    },
 };
