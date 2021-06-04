18710bd82782bbc1ea2e1b642cc046c9c828ebb5
Pablo Cesar <browntth@icloud.com>
2021-06-03 20:14:08 -0500

    4.5.11.4
    agregue la funcionalidad para que un ponente pueda reportar un problema de un curso en el escenario1 y todo el backend para reportar cualquier problema con el sistema, solo hay que poner las rutas en los hbs

--- componentes/ponente/network.js
+++ componentes/ponente/network.js
@@ -12,6 +12,9 @@ router.get('/soporte', render.getSoporte);
 router.post('/agregarExamen', render.postAgregarExamen);
 router.get('/curso/:idCurso', render.getConsultarCurso);
 router.post('/AgregarTarea', crearCarpetaTarea, upload.array('multimediaTarea'), render.postAgregarTarea);
+
+router.post('/reportarProblemaCurso', render.postReportarProblemaCurso);
+router.post('/reportarProblemaUsuario', render.postReportarProblemaUsuario);
 /* router.get('/AgregarTareaPrueba', render.getAgregarTarea);
 router.post('/AgregarTarea', upload.array('multimediaTarea'), render.postAgregarTarea);
 router.get('/DescargarTarea', render.getDescargarArchivos); */
--- componentes/ponente/render.js
+++ componentes/ponente/render.js
@@ -7,14 +7,14 @@ module.exports = {
 
     getDashboardPonente: async function (req, res, next) {
         const usuarioID = (req.user.id);
-        const cursosActivos= await Controller.listCursosActuales(usuarioID); 
+        const cursosActivos = await Controller.listCursosActuales(usuarioID);
         // const usuarioID = 1;
         // const cursosActivos= await Controller.listCursosActivos(usuarioID); 
         const HistorialCursosPonente = await Controller.getHistorialCursosPonente(req.user.id);
         const cursosHistorial = HistorialCursosPonente.length
         const cursosActuales = cursosActivos.length
         res.render('ponente/p1_dashboard', {
-            ponente:true,
+            ponente: true,
             // cursosActivos,
             HistorialCursosPonente,
             cursosHistorial,
@@ -29,7 +29,6 @@ module.exports = {
         const documentosCurso = await Controller.listDocumentos(req.params.idCurso);
         const curso = datosCurso[0];
         const asignacionesPonente = await Controller.listAsignacionesPonente(req.params.idCurso);
-    
         res.render('ponente/p2_consultarCursoE1_v2', {
             ponente: true,
             curso,
@@ -40,7 +39,7 @@ module.exports = {
             asignacionesPonente,
         });
     },
-    
+
     getConsultarCursoPE2: async function (req, res, next) {
         res.render('ponente/p2_consultarCursoE2_v2', {
             ponente: true
@@ -50,9 +49,9 @@ module.exports = {
         const estudiantes = await Controller.listEstudiantes();
         const modalEstudiante = await Controller.listEstudiantes();
         res.render('ponente/p3_consultarAlumnos', {
-            ponente:true,
-            datatables:true,
-            dataTablesExport:true,
+            ponente: true,
+            datatables: true,
+            dataTablesExport: true,
             modalEstudiante,
             estudiantes,
         });
@@ -67,7 +66,7 @@ module.exports = {
             ponente: true
         });
     },
-    postAgregarExamen: async function (req, res, next){
+    postAgregarExamen: async function (req, res, next) {
         await Controller.insertExamen(req.body);
         res.redirect('/ponente/consultarCursoPEI')
     },
@@ -78,11 +77,31 @@ module.exports = {
         for (const file of req.files) {
             const nombreMultimedia = file.originalname;
             const linkMultimedia = `${tareaID}\\\\${nombreMultimedia}`;
-            datos = {tareaID, nombreMultimedia, linkMultimedia};
+            datos = { tareaID, nombreMultimedia, linkMultimedia };
             await Controller.insertMultimediaTarea(datos);
         }
         res.redirect('/ponente/consultarCursoPEI')
     },
+
+    postReportarProblemaCurso: async function (req, res, next) {
+        await Controller.reportarProblemaCurso(
+            req.user.id,
+            req.body.idCurso,
+            req.body.asuntoProblema,
+            req.body.descripcionProblema
+        );
+        res.redirect('/ponente/curso/'+req.body.idCurso);
+    },
+
+    postReportarProblemaUsuario: async function (req, res, next) {
+        await Controller.reportarProblemaUsuario(
+            req.user.id,
+            //el resto de datos los leemos del modal
+            req.body.asuntoProblema,
+            req.body.descripcionProblema
+        );
+        //de donde se manda llamar?, para dirigirlo allí
+    }
 
     /* getAgregarTarea: async function(req, res, next){
         res.render('speaker/AgregarTarea')
