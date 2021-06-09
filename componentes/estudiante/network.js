4688eabe9c57aea83e588c44a709abcecb380961
Pablo Cesar <browntth@icloud.com>
2021-06-09 13:38:12 -0500

    5.5.3.1
    se descarta temporalmente la ruta de soporte por que está siendo modificada, no está completa

--- componentes/estudiante/network.js
+++ componentes/estudiante/network.js
@@ -9,7 +9,7 @@ router.get('/curso/:idCurso', render.getConsultarEstadoCursoEstudiante);
 router.get('/consultarCursoEII/', render.getConsultarCursoE2);
 router.get('/consultarCursoEIII', render.getConsultarCursoE3);
 router.post('/Inscribirse', render.postInscribirse)
-router.get('/soporte', render.getSoporte, render.postEnviarReporte);
+//router.get('/soporte', render.getSoporte, render.postEnviarReporte);
 router.get('/DescargarTemario/:idCurso', render.getDescargarTemario);
 /* router.get('/', render.getVerTareas);
 router.get('/:id', render.getVerTarea);
